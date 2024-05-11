e933037a3743b200dcc1fb62110365286d407ca1
moha_amini <mhaminii18@gmail.com>
2024-05-11 18:44:52 +0330

    setting up database and server + prittier config

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const { red, green, blue, magenta } = require('console-log-colors')
+const express = require('express')
+const cors = require('cors');
+const cookies = require("cookie-parser");
+const morgan = require('morgan')
+
+const sequelize = require("./config/db")
+
+const app = express()
+
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('combined')) 
+app.use(cookies());
+app.use(cors());
+
+const PORT = process.env.PORT || 8080
+
+;(async () => {
+    try {
+        await sequelize.authenticate()
+        console.log(
+            magenta('DB: Connection has been established successfully.')
+        )
+        // await sequelize.sync({ force: true })
+        await sequelize.sync({ alter: true })
+
+        console.log(magenta('DB: All models were synchronized successfully.'))
+        // Listen on pc port
+        app.listen(PORT, () =>
+            console.log(blue(`Server running on PORT ${PORT}`))
+        )
+    } catch (error) {
+        console.error(red('DB: Unable to connect to the database:', error))
+    }
+})()
+
+// check server up
+app.get('/', async (req, res) => {
+    try {
+        res.status(200).json({message:"Welcome!"})
+    } catch (error) {
+        // Handle any errors (e.g., validation errors, database errors)
+        res.status(400).json({ error: error.message })
+    }
+})
